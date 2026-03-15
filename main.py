2ecfb0aece098d78604addceca23fc4e36edc5f0
CEP2 <clifporter@gmail.com>
2026-03-14 22:07:11 -0400

    logger content added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
+import constants
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {constants.SCREEN_WIDTH}\n Screen height: {constants.SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
